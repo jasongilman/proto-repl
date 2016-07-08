5b69d3f987d30b6b9f9b4b143294f18f9d25f91c
Jason Gilman <jason@element84.com>
2016-07-07 20:22:59 -0400

    #111 - Adding clojure.tools.namespace as a library dependency of proto repl so it will be available in most Proto REPL projects.

--- proto-no-proj/project.clj
+++ proto-no-proj/project.clj
@@ -1,9 +1,8 @@
 (defproject proto-no-proj "0.1.0-SNAPSHOT"
   :description "A default leiningen project to use when there is no project."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [org.clojure/tools.namespace "0.2.11"]
                  [proto-repl-charts "0.3.0"]
-                 [proto-repl "0.2.0"]]
+                 [proto-repl "0.2.1"]]
 
   :profiles
   {:dev {:source-paths ["dev" "src"]}})
--- proto-repl-lib/project.clj
+++ proto-repl-lib/project.clj
@@ -1,15 +1,15 @@
-(defproject proto-repl "0.2.0"
+(defproject proto-repl "0.2.1"
   :description "A set of helper functions for projects used in Proto REPL"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/core.async "0.2.374"]
+                 [org.clojure/tools.namespace "0.2.11"]
                  ;; For completions
                  [compliment "0.2.7"]]
 
   :profiles
-  {:dev {:dependencies [[org.clojure/tools.namespace "0.2.11"]
-                        [pjstadig/humane-test-output "0.7.1"]]
+  {:dev {:dependencies [[pjstadig/humane-test-output "0.7.1"]]
          :injections [(require 'pjstadig.humane-test-output)
                       (pjstadig.humane-test-output/activate!)]
          :source-paths ["dev" "src" "test"]}})
