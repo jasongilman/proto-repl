f181147500c63fd5e60778e123093387c32abea5
Jason Gilman <jason@element84.com>
2016-07-18 22:32:26 -0400

    Updating the versions of proto-repl and proto-repl-charts in the default leiningen project.- Fixed

--- proto-no-proj/project.clj
+++ proto-no-proj/project.clj
@@ -1,8 +1,8 @@
 (defproject proto-no-proj "0.1.0-SNAPSHOT"
   :description "A default leiningen project to use when there is no project."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl-charts "0.3.0"]
-                 [proto-repl "0.3.1"]]
+                 [proto-repl-charts "0.3.1"]
+                 [proto-repl "0.3.0"]]
 
   :profiles
   {:dev {:source-paths ["dev" "src"]}})
