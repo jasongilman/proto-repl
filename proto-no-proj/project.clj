79304c45e2606aa766fd3ea955dc2dd4fa51f62a
Jason Gilman <jason@element84.com>
2016-07-27 19:54:30 -0400

    Updating default proto repl project to use the latest version of the proto repl library.

--- proto-no-proj/project.clj
+++ proto-no-proj/project.clj
@@ -2,7 +2,7 @@
   :description "A default leiningen project to use when there is no project."
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [proto-repl-charts "0.3.1"]
-                 [proto-repl "0.3.0"]]
+                 [proto-repl "0.3.1"]]
 
   :profiles
   {:dev {:source-paths ["dev" "src"]}})
