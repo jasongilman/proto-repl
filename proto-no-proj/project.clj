7941f2beae360215bbc52c45fef5f64f6dbc6fcf
Jason Gilman <jason@element84.com>
2016-11-23 19:42:30 -0500

    Updating styling for presentation mode

--- proto-no-proj/project.clj
+++ proto-no-proj/project.clj
@@ -1,7 +1,7 @@
 (defproject proto-no-proj "0.1.0-SNAPSHOT"
   :description "A default leiningen project to use when there is no project."
   :dependencies [[org.clojure/clojure "1.8.0"]
-                 [proto-repl-charts "0.3.1"]
+                 [proto-repl-charts "0.3.3-presentation"]
                  [proto-repl "0.3.1"]]
 
   :profiles
