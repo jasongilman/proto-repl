14c50a9cfd16360a5675b8cad737c47f6d18821c
Torbjørn Vatn <torbjorn.vatn@unacast.com>
2016-12-17 08:51:28 +0100

    Remove the SNAPSHOT from version

--- lein-template/src/leiningen/new/proto_repl/project.clj
+++ lein-template/src/leiningen/new/proto_repl/project.clj
@@ -1,4 +1,4 @@
-(defproject {{ns-name}} "0.1.0-SNAPSHOT"
+(defproject {{ns-name}} "0.1.0"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
